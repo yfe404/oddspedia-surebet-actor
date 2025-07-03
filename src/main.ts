90cd10b4b4eb5df8b3f330718aa97cecf86741bb
yfe404 <yfe.github@protonmail.com>
2025-07-03 15:08:40 +0200

    feat(main.ts): add countryCode 'FR' to proxy configuration for improved geolocation support

--- src/main.ts
+++ src/main.ts
@@ -36,6 +36,7 @@ let proxyConfiguration = undefined;
 if (Actor.isAtHome()) {
         proxyConfiguration = await Actor.createProxyConfiguration({
         groups: ['RESIDENTIAL'],
+        countryCode: 'FR'
     });
 }
 
